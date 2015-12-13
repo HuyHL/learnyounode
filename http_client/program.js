869268a1f0eded01daf6adb9ec6e1a7afa864c86
HuyHL <hpit.vie@gmail.com>
2015-12-13 21:33:25 +0700

    http client

---
+++ http_client/program.js
@@ -0,0 +1,13 @@
+var httpClient = require('http');
+
+var urlPath = process.argv[2];
+
+httpClient.get(urlPath, callback);
+
+function callback(response) {
+    response.setEncoding('utf-8');
+    response
+        .on("data", function(data){
+            console.log(data);
+        });
+}
